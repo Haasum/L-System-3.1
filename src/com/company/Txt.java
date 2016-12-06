bf6a726db926205ad83591c4b71544f9df532a12
Nanna <nnz@ruc.dk>
2016-12-06 15:07:27 +0100

    Nanna har ændret i txt.klassen

--- src/com/company/Txt.java
+++ src/com/company/Txt.java
@@ -1,18 +1,46 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by MasterWillis on 03/12/2016.
  */
 public class Txt {
-    public String inputFromTxt = "A:F[+A][-A],b:ACA";
+    //public String inputFromTxt = "A:F[+A][-A],b:ACA";
 
-    public Txt(){
+    public Txt() throws IOException {
+        checkTxt();
+    }
 
+    public String checkTxt() throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("C:\\Users\\nanna\\IdeaProjects\\Proto2\\Proto2\\src\\LsysRecursive\\testTxt.txt"));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                try {
+                    line = br.readLine();
+                } catch
+                        (IOException e) {
+                    e.printStackTrace();
+                }
+                String inputFromText = sb.toString();
+                System.out.println("textfilen er " + inputFromText + "woop woop");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
-
-    public String getTxtInput() { //her er outputtet samlet efter randommetoder er kørt
-        return this.inputFromTxt; // når input fra textFil skal virke
-    }
+    //public String getTxtInput() { //her er outputtet samlet efter randommetoder er kørt
+        //return this.inputFromTxt; // når input fra textFil skal virke
 }
