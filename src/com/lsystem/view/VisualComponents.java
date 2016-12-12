daeadc632e1c9bc76be19bcc9ddf865b69c3f4e5
najacm <naja.marckwardt@gmail.com>
2016-12-12 12:12:02 +0100

    skrevet navn på knapper i menupanel

--- src/com/lsystem/view/VisualComponents.java
+++ src/com/lsystem/view/VisualComponents.java
@@ -1,7 +1,6 @@
 package com.lsystem.view;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,17 +13,13 @@ public class VisualComponents {
     public static Image background;
     public static Image leafRight;
     public static Image leafLeft;
-    public static BufferedImage textureOff;
-    public static Image textureOn;
     private BufferedImage barkImg;
     public static TexturePaint barkTex;
-    public static ImageIcon texOffIcon;
 
     public VisualComponents(boolean shouldLoad) {
         if (shouldLoad ==  true) {
             loadImages();
             barkTex = new TexturePaint( this.barkImg, new Rectangle(0, 0, 150, 150));
-            texOffIcon = new ImageIcon(this.textureOff);
         }
         else {
             setImagestoNull();
@@ -46,8 +41,6 @@ public class VisualComponents {
             this.leafRight = tk.createImage("Src//image_package//leafRight.png");
             this.leafLeft = tk.createImage("Src//image_package//leafleft.png");
             this.barkImg = ImageIO.read(new File("Src//image_package//barkTexture.jpg"));
-            this.textureOff = ImageIO.read(new File("Src//image_package//textureoff.png"));
-            this.textureOn = ImageIO.read(new File("Src//image_package//textureOn.png"));
         } catch (IOException var2) {
             Logger.getLogger(VisualComponents.class.getName()).log(Level.SEVERE, (String)null, var2);
         }
