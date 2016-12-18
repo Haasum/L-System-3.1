7d0f70895f8512214b16f4d476cc7e8afc9950ae
najacm <naja.marckwardt@gmail.com>
2016-12-18 14:05:10 +0100

    NonTerminal har nu javadocs kommentarer. dog mangler jeg at udfylde parametre

--- src/com/lsystem/view/MenuPanel.java
+++ src/com/lsystem/view/MenuPanel.java
@@ -35,8 +35,13 @@ public class MenuPanel extends JPanel {
         makeInputButtons();
     }
 
-
-
+    /**
+     * Creates the input buttons.
+     * <p>
+     *     This creates the input buttons, which are used to choose the preferred input for the user.
+     *     This also creates the actionlistener for the inputbuttons.
+     * </p>
+     */
     private void makeInputButtons() { //TODO: Til leapmotion - må ikke slettes
         JLabel inputLabel = new JLabel("CHOSE INPUT");
         inputLabel.setLocation(MENU_MIDDLE-40, 230);
@@ -44,7 +49,7 @@ public class MenuPanel extends JPanel {
         this.add(inputLabel);
         int y = 200;
 
-        for (InputType it : InputType.values()) {
+        for (InputType it : InputType.values()) { //for-each loop which creates a button for each inputType.
             y += 60;
 
             Button inputbutton = new Button(String.valueOf(it));
@@ -53,18 +58,25 @@ public class MenuPanel extends JPanel {
             inputbutton.setSize(100, 50);
             this.add(inputbutton);
 
-            inputbutton.addActionListener(new ActionListener() {
+            inputbutton.addActionListener(new ActionListener() { //adds a new actionlistener for each button
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    UserInputFactory userInputFactory = new UserInputFactory(lsys);
-                    userInput = userInputFactory.fetchUserInput(it);
-                    staticView.addListeners(userInput);
+                    UserInputFactory userInputFactory = new UserInputFactory(lsys); //initialize the user input factory
+                    userInput = userInputFactory.fetchUserInput(it); //the userinput-type is fetched
+                    staticView.addListeners(userInput); //the choosen input type is added to the static view
                 }
             });
         }
     }
 
-
+    /**
+     * Creates texture buttons.
+     * <p>
+     *     This creates the two buttons "TEXTURE ON" and "TEXTURE OFF".
+     *     The buttons contains an actionlistener, which changes the visualcomponents,
+     *     thereby removing or applying texture to the view.
+     * </p>
+     */
     private void makeTextureButtons() {
 
         JLabel textureLabel = new JLabel("TEXTURE ON/OFF");
@@ -72,30 +84,28 @@ public class MenuPanel extends JPanel {
         textureLabel.setSize(100, 30);
         this.add(textureLabel);
 
-        Button textureOnBtn = new Button("TEXTURE ON");
+        Button textureOnBtn = new Button("TEXTURE ON"); //Creates the "TEXTURE ON" button
         textureOnBtn.setLocation(MENU_MIDDLE-50, 100);
         textureOnBtn.setBackground(Color.lightGray);
         textureOnBtn.setSize(100, 50);
         this.add(textureOnBtn);
 
-        textureOnBtn.addActionListener(new ActionListener() {
+        textureOnBtn.addActionListener(new ActionListener() { //Actionlistener for the button
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                visualComponents = new VisualComponents(true);
+                visualComponents = new VisualComponents(true); //creates af ned instance of the Visual componentClass
             }
         });
 
-        Button textureOffBtn = new Button("TEXTURE OFF");
+        Button textureOffBtn = new Button("TEXTURE OFF"); //creates the "TEXTURE OFF" button
         textureOffBtn.setBackground(Color.LIGHT_GRAY);
         textureOffBtn.setLocation(MENU_MIDDLE-50, 160);
         textureOffBtn.setSize(100, 50);
         this.add(textureOffBtn);
-        textureOffBtn.addActionListener(new ActionListener() {
+        textureOffBtn.addActionListener(new ActionListener() { //actionlistener for the button
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                visualComponents = new VisualComponents(false);
+                visualComponents = new VisualComponents(false);//creates af ned instance of the Visual componentClass
             }
         });
     }
