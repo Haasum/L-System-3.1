76d2e10032e965dd3dafb690aecff6cdc3260e78
najacm <naja.marckwardt@gmail.com>
2016-12-19 09:22:14 +0100

    Har lavet javadocs for: DynamiView MenuPanel NonTerminal StaticView Grammatik RandomRulePicker
    Igang med: RecursiveLsys
    
    Overfladisk javadoc og mangler kommentarer på contructor og sikkert også en del inline kommentarer. HAr dog fokuseret på javadocs over hver eneste metode, da det er det vigtigste

--- src/com/lsystem/model/RecursiveLsystem.java
+++ src/com/lsystem/model/RecursiveLsystem.java
@@ -23,10 +23,23 @@ public class RecursiveLsystem {
     }
 
     /**
-     * Expands the current string.
+     * Expands the current string recursively.
      * <p>
      *     This expands the string, with the rules from the ruleset array.
-     *     //TODO: nået her til
+     *     This contains of a for-loop, that runs through a String (s), and expands that String.
+     *
+     *     The for-loop checks the characters in the String one by one.
+     *     First the characters rule is returned by the method getRuleInRuleset, //method or function
+     *     which could of one of two outcomes:
+     *     <p>1. outcome: If the character is a primitive (e.g. '+'), the getRuleInRuleset method returns null.
+     *     If so the character is not replaced by a rule, and will just be applied directly to the expanded String.
+     *     2. outcome: If the character isnt a primitive (e.g. 'A'), the getRuleInRuleset method returns the expansion
+     *     rule of that character (e.g. F[+A][-A]). The rule (not the character) is then applied to the expanded String.
+     *
+     *     //TODO: mere med expand igen. recursion.
+     *     </p>
+     *
+     *
      * </p>
      * @param s is the string to be expanded
      * @param genNo is the number of times the string should be expanded
@@ -39,19 +52,33 @@ public class RecursiveLsystem {
         if (genNo == 0) {
             return s;
         }
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) { //the for-loop that runs through the String character by character
             char c = s.charAt(i);
-            String ruleToExpand = getRuleInRuleset(c);
-            if (ruleToExpand != "" && ruleToExpand != null) {
-                next += expand(ruleToExpand, genNo-1  );
+            String ruleToExpand = getRuleInRuleset(c); //expands the character by calling the getRuleInRuleset function
+            if (ruleToExpand != "" && ruleToExpand != null) { //if the character has a rule, this if sentence counts
+                next += expand(ruleToExpand, genNo-1  ); //whats to be expanded next
             }
-            else if (ruleToExpand == "" || ruleToExpand == null) {
+            else if (ruleToExpand == "" || ruleToExpand == null) { //if the character is a primitive, this if sentence counts.
                 next += c;
             }
         }
         return next;
     }
 
+    /**
+     * Fetches the left side of the ruleset for the current character.
+     * <p>
+     *     This gets a parameter - the current character from the previous method.
+     *     The purpose of this method is to fetch the expansion rule of that character.
+     *     First the method looks op the character in the ruleset Array.
+     *     Secondly, if the character has two or more rules, the RandomRulePicker class is initialized
+     *     and a random rule is chosen from the array of rules. That chosen rule is returned.
+     *
+     *     If the character only if one rule, that rule is the String (s), that this method returns
+     * </p>
+     * @param c the current character to be expanded
+     * @return the expansion-rule of that current character
+     */
     public String getRuleInRuleset(char c) {
         String s = "";
 
