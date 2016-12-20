87eb8b2f26c28739e0b25905a321b88b82c23742
najacm <naja.marckwardt@gmail.com>
2016-12-20 19:41:21 +0100

    HAR LEAP

--- src/com/lsystem/Main.java
+++ src/com/lsystem/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /*TODO: denne udkommentering bruges til leapmotion
-         Create a sample listener and controller*/
+
+
        final com.leapmotion.leap.Controller controller2 = new com.leapmotion.leap.Controller();
         Controller control = new Controller(controller2);
 
